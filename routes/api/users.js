d3bb9c75b185d69d7e63c24f557bd9f911fc52cf
AliciaJz <zilaphono@gmail.com>
2018-06-09 20:49:02 -0500

    jwt auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 // Load User model
 const User = require("../../models/User");
@@ -56,36 +57,49 @@ router.post("/login", (req, res) => {
   const password = req.body.password;
 
   // Find user by email
-  User.findOne({ email })
-    .then(user => {
-      // Check for user
-      if (!user) {
-        return res.status(400).json({ email: "User not found" });
-      }
-      // Check password
-      bcrypt.compare(password, user.password).then(isMatch => {
-        if (isMatch) {
-          // User matched
-          const payload = { id: user.id, name: user.name, avatar: user.avatar }; // create jwt payload
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(400).json({ email: "User not found" });
+    }
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // create jwt payload
 
-          // Sign the token
-          jwt.sign(
-            payload,
-            keys.secretOrKey,
-            { expiresIn: 3600 },
-            (err, token) => {
-              res.json({
-                success: true,
-                token: "Bearer " + token
-              });
-            }
-          );
-        } else {
-          return res.status(400).json({ password: "Password incorrect" });
-        }
-      });
-    })
-    .catch();
+        // Sign the token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
 });
+
+// @route       GET api/users/current
+// @description Return current user
+// @access      Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
